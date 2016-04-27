4ed135d8ca2c988f06b2695d6381e10a05139c04
Sia Gholami <myself@seeafish.me>
2016-04-27 17:37:16 +0100

    Switch off all lights and enable the one required

--- spio.go
+++ spio.go
@@ -3,18 +3,34 @@ package jim
 import (
 	"fmt"
 	"github.com/stianeikeland/go-rpio"
-	"os"
+	"time"
 )
 
-func main() {
+func change_light(state int64) {
 	if err := rpio.Open(); err != nil {
 		fmt.Println(err)
-		os.Exit(1)
 	}
 	defer rpio.Close()
 
-	pin = rpio.Pin(7)
+	pins := make(map[int64]int64)
+	pins[5] = 11
+	pins[4] = 7
+	pins[3] = 10
+	pins[2] = 19
+	pins[1] = 22
+
+	for _, v := range pins {
+		pin := rpio.Pin(v)
+		pin.Output()
+		pin.High()
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	pin := rpio.Pin(pins[state])
 	pin.Output()
-	time.Sleep(5 * time.Second)
-	pin.Toggle()
+	pin.Low()
 }
+
+// func main() {
+// 	change_light(5)
+// }
