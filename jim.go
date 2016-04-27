0bc0bf8788837e177a62884277daed49a77259ea
Cian Brennan <cian.brennan@rackspace.co.uk>
2016-04-27 11:13:04 +0100

    fix some errors

--- jim.go
+++ jim.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 )
 
-func check_auth(h, Header) {
+func check_auth(h, Header) boolean {
 	return true
+}
+
+func change_light(i int) error {
+	return nil
 }
 
 func lightHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,12 +19,12 @@ func lightHandler(w http.ResponseWriter, r *http.Request) {
 	if !authenticated {
 		http.Error(w, "Not authenticated", 403)
 	}
-	light_str = r.URL.Path[len("/api/light/")]
-	light_int, err = strconv.Atoi(light)
+	light_str := r.URL.Path[len("/api/light/")]
+	light_int, err := strconv.Atoi(light)
 	if err != nil {
 		http.Error(w, err.Error, 500)
 	}
-	err = change_light(light_int)
+	err := change_light(light_int)
 	if err != nil {
 		http.Error(w, err.Error, 500)
 	}
